9073d291d131eb3e3b69522882d1187a37ea02fa
syntel <huntmac10@gmail.com>
2018-05-24 15:48:53 -0400

    Configured mysql connection.  Created search component and removed react boilerplate from create react app

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const creds = require('../config/creds');
+const connection = mysql.createConnection(creds);
+connection.connect();
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
