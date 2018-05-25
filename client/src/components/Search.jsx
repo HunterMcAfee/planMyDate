006c404a17eeba1decd5eb4064f2fea30abe9292
syntel <huntmac10@gmail.com>
2018-05-25 10:53:48 -0400

    Got Google Maps Api working and got rid of CORS issues.  Search component is now able to make a post request that will search for the query string

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import api_key from '../config/api_key';
 
 class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchData: []
+        }
+    }
+
+    handleSearch(event) {
+        event.preventDefault();
+        const searchTerm = event.target[0].value;
+        axios.post('http://localhost:3005/api/search', {
+            "searchTerm": searchTerm
+            })
+            .then((res) => {
+                console.log(res.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
     render() {
         return (
             <div>
-                <form>
-                    <input type="text" />
+                <form onSubmit={this.handleSearch}>
+                    <input type="text" placeholder="Type in your search terms here" name="searchTerm" />
+                    <button className="btn btn-primary">Search</button>
                 </form>
             </div>
         )
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 const creds = require('../config/creds');
+const api_key = require('../config/api_key');
 const connection = mysql.createConnection(creds);
 connection.connect();
+const axios = require('axios');
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-// });
+router.post('/search', function(req, res, next) {
+    const textSearch = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.body.searchTerm}&key=${api_key}`;
+    axios.get(textSearch)
+            .then((responseData) => {
+                res.json(responseData.data);
+            })
+            .catch((error) => {
+                res.json(error);
+                console.log(error);
+            })
+});
 
 module.exports = router;
