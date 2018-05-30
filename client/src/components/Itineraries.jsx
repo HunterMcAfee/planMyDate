0f89fdcd182a2ead26c9d6f01b06270e5dd92184
knosence <nadario.seays@gmail.com>
2018-05-29 21:15:29 -0400

    Added more styles to itineraries register, and login,

--- client/src/components/Itineraries.jsx
+++ client/src/components/Itineraries.jsx
@@ -4,10 +4,25 @@ import { Link } from 'react-router-dom';
 
 const Itinerary = (props) => {
     return (
-        <div>
-            <div>{props.itinerary.name}</div>
-            <div>{props.itinerary.summary}</div>
-            <Link to={`/itineraries/${props.userId}/itinerary/${props.itinerary.itinerary_id}`}><button>Go to</button></Link>
+        <div className = "row itinerary-box">
+            <div className = "col-sm-3">
+                <label> Itinerary Name: </label>
+                <br />
+                {props.itinerary.name}
+            </div>
+            <div className = "col-sm-3 word-wrap">
+                <label> Itinerary Summary: </label>
+                <br />
+                {props.itinerary.summary}
+            </div>
+            <div className = "col-sm-3 word-wrap">
+                <label> Itinerary Date: </label>
+                <br />
+                {props.itinerary.date}
+            </div>
+            <div className="col-sm-3 text-center">
+                <Link to={`/itineraries/${props.userId}/itinerary/${props.itinerary.itinerary_id}`}><button className="btn btn-primary">Go to</button></Link>
+            </div>
         </div>
     )
 }
@@ -35,9 +50,17 @@ class Itineraries extends Component {
 
     render() {
         return (
-            <div>
+            <div className="container">
+                <div className = "col-md-12 text-center header">
                 <h1>Your Itineraries</h1>
-                <Link to={`/itineraries/${this.props.match.params.userId}/createItinerary`}><button className="btn btn-primary">Create</button></Link>
+                </div>
+                <div className="row">
+                    <div className = "col-md-12 text-center form">
+                        <Link to={`/itineraries/${this.props.match.params.userId}/createItinerary`}>
+                            <button className="btn btn-primary">Create New Itinerary</button>
+                        </Link>
+                    </div>
+                </div>
                 {this.state.itineraries.map((itinerary, i) => {
                     return(
                         <Itinerary userId={this.props.match.params.userId} itinerary={itinerary} key={i} />
